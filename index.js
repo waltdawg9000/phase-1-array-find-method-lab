19cde7a751ed2f722228b9521c074cc48c68d744
Walter Morris <waltercmorris18@gmail.com>
2021-12-31 02:21:42 -0800

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function superbowlWin(array) {
+    const results = array.find(record => record.result === "W") 
+    
+    if (results === true) {
+                 return results.year
+     } else {
+         return undefined
+    }
+ 
+}
